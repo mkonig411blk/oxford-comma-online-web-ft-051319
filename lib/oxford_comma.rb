135e4b2abbb325efddcc2b066297dd5026a73f7f
Margot Konig <margot.konig@blackrock.com>
2019-05-15 03:34:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  i = 0
+  string = ""
+if array.length == 1
+  return array[0]
+elsif array.length == 2
+  array[0] << " and "
+array.join()
+else 
+  until i == array.length - 1
+      string << "#{array[i]}, "
+      i += 1
+    end
+    string << "and #{array[array.length-1]}"
+    return string
 end
+end
+
