fe49e9a45306368f8648208ab4eb3ece90093963
Margot Konig <margot.konig@blackrock.com>
2019-05-15 03:34:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,21 +6,13 @@ if array.length == 1
 elsif array.length == 2
   array[0] << " and "
 array.join()
-elsif array.length > 2 
-array[array.length + 1] << "and"
-array.join (", ")
-else array.length < 2
-  array.join()
-end
-end
-
-def oxford_comma(array)
-
-    until i == array.length - 1
+else 
+  until i == array.length - 1
       string << "#{array[i]}, "
       i += 1
     end
     string << "and #{array[array.length-1]}"
     return string
-  end
 end
+end
+
